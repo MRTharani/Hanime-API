e1a3cea04dbc4a5d9fea4944c810cacacfa2c0e8
MRTharani <tharani@mrspidyxd.work.gd>
2024-10-05 06:03:53 +0000

    commit

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import os
+import asyncio
 from yt_dlp import YoutubeDL
 from scaper import *
+from tools import generate_thumbnail
+from upload import switch_upload,upload_thumb
+from database import *
+import logging
+from config import *
+
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+db = connect_to_mongodb(MONGODB_URI, "Spidydb")
+collection_name = COLLECTION_NAME
+
+if db is not None:
+    logging.info("Connected to MongoDB")
+
+
+
 
 def download_video(url, output_file):
     ydl_opts = {
@@ -11,7 +30,7 @@ def download_video(url, output_file):
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-def main():
+async def main():
     links = []
     commands = [
         ["htv", "new-uploads", "-u", "-a"],
@@ -24,8 +43,24 @@ def main():
         links.extend(fetch_video_links(command))
 
     for title, url in links:
-        output_file = f'{title}.mp4'
-        download_video(url, output_file)
+        downloads = [ download["Name"] for download in find_documents(db, collection_name)] 
+        if title not in downloads:
+            output_file = f'{title}.mp4'
+            output_thumb = f'{title}.png'
+            download_video(url, output_file)
+            generate_thumbnail(output_file, output_thumb)
+            img = await upload_thumb(output_thumb)
+            msg = await switch_upload(output_file,output_thumb)
+            media_link = msg.media_link
+            img_link = img.media_link
+            document = {"Name":title,"Raw_Url":url,"Video":media_link,"Image":img_link}
+            insert_document(db, collection_name, document)
+            if os.path.exists(output_file):
+                os.remove(output_file)
+            if os.path.exists(output_thumb):
+                os.remove(output_thumb)
+        else:
+            logging.info(f"{title} is Already Downloaded")
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
---
+++ tools.py
@@ -0,0 +1,70 @@
+import os
+import subprocess
+import shlex
+import time
+import sys
+import logging
+import random
+
+
+
+
+
+def generate_thumbnail(file_name, output_filename, retry_interval=10, max_retries=10):
+    retries = 0
+    while retries < max_retries:
+        if os.path.exists(file_name):
+            video_duration = get_video_duration(file_name)
+            if video_duration is None:
+                logging.error("Could not retrieve video duration.")
+                return False
+            
+            random_time = random.uniform(200, video_duration)
+            random_time_str = time.strftime('%H:%M:%S', time.gmtime(random_time))
+    
+            command = ['ffmpeg', '-ss', random_time_str, '-i', file_name, '-vframes', '1', output_filename]
+            try:
+                subprocess.run(command, check=True, capture_output=True, text=True)
+                logging.info(f"Thumbnail saved as {output_filename} at {random_time_str}")
+                return True
+            except subprocess.CalledProcessError as e:
+                logging.error(f"Error: {e}")
+                logging.error(f"Command output: {e.output.decode()}")
+                return False
+        else:
+            logging.info(f"File {file_name} does not exist. Retrying in {retry_interval} seconds...")
+            time.sleep(retry_interval)
+            retries += 1
+
+    logging.error(f"{max_retries} retries.")
+    return False
+
+
+def print_progress_bar(name, downloaded, total_size, length=20):
+    if total_size <= 0:
+        total_size = downloaded
+    percent = ("{0:.1f}").format(100 * (downloaded / float(total_size)))
+    filled_length = int(length * downloaded // total_size)
+    bar = '#' * filled_length + '-' * (length - filled_length)
+    sys.stdout.write(f'\r{name} - [{bar}] {percent}%')
+    sys.stdout.flush()
+
+
+def format_bytes(byte_count):
+    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    index = 0
+    while byte_count >= 1024 and index < len(suffixes) - 1:
+        byte_count /= 1024
+        index += 1
+    return f"{byte_count:.2f} {suffixes[index]}"
+
+def get_video_duration(file_name):
+    command = ['ffmpeg', '-i', file_name, '-hide_banner']
+    result = subprocess.run(command, stderr=subprocess.PIPE, text=True)
+    duration_line = [x for x in result.stderr.split('\n') if 'Duration' in x]
+    if duration_line:
+        duration = duration_line[0].split()[1]
+        h, m, s = duration.split(':')
+        total_seconds = int(h) * 3600 + int(m) * 60 + float(s[:-1])
+        return total_seconds
+    return None
